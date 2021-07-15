f249cbe1b62bfef5734d368849c8614aa452c4f8
LemonPocky <someone-70@live.com>
2021-07-14 20:41:56 -0700

    WIP Added notes saving and loading

--- apiroutes/notes.js
+++ apiroutes/notes.js
@@ -1,12 +1,23 @@
 // Required to use path.join
 const path = require('path');
 
+// Manages our user data
+const NoteData = require('../src/NoteData.js');
+const noteData = new NoteData();
+
+// Called once on server start
 function loadNotesApi(app) {
-  app.get('/api/notes');
+  // Load all notes from "database"
+  noteData.loadAll();
+  app.get("/api/notes", (req, res) => {
+    res.json(noteData.getNotes());
+  });
+
+  app.post("/api/notes", (req, res) => {
+    // Parse the request to get a JSON object
+    const note = JSON.parse(req.body);
+    noteData.save(note);
+  })
 }
-
-function getNotes() {}
-
-function saveNote(note) {}
 
 module.exports = loadNotesApi;
---
+++ src/NoteData.js
@@ -0,0 +1,33 @@
+// Needed to read/write data to files
+const fs = require('fs').promises;
+// Required to use path.join
+const path = require('path');
+// Generates unique ids for notes
+const { nanoid } = require('nanoid');
+
+const ID_LENGTH = 10;
+
+class NoteData {
+  constructor() {
+    const notes = [];
+  }
+
+  async loadAll() {
+    const data = await fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8')
+    this.notes = JSON.stringify(data);
+  }
+  
+  getNotes() {
+    return this.notes;
+  }
+
+  async save(note) {
+    if (!note.id) {
+      note.id = nanoid(ID_LENGTH);
+    }
+    this.notes.push(note);
+    await fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(this.notes));
+  }
+}
+
+module.exports = NoteData;
