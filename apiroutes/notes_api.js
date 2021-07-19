3dc9d05f179052f11d0e897ec96e051ddd09286b
LemonPocky <someone-70@live.com>
2021-07-19 16:08:02 -0700

    Implemeneted DELETE route for /api/notes/:id

--- apiroutes/notes_api.js
+++ apiroutes/notes_api.js
@@ -9,10 +9,13 @@ const noteData = new NoteData();
 function loadNotesApi(app) {
   // Load all notes from "database"
   noteData.loadNotesFromFile();
+
+  // GET all notes
   app.get("/api/notes", (req, res) => {
     res.json(noteData.getNotes());
   });
 
+  // POST and save a new note
   app.post("/api/notes", (req, res) => {
     // Parse the request to get a JSON object
     const note = req.body;
@@ -23,7 +26,24 @@ function loadNotesApi(app) {
       .finally(() => {
         res.end();
       })
-  })
+  });
+
+  // DELETE a note based on the id given
+  // If the note doesn't exist, returns a 404
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    noteData.delete(id)
+      .catch((error) => {
+        if (error.message === `Note ID not found.`) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(500);
+        }
+      })
+      .finally( () => {
+        res.end();
+      });
+  });
 }
 
 module.exports = loadNotesApi;
--- src/NoteData.js
+++ src/NoteData.js
@@ -17,6 +17,7 @@ class NoteData {
     this.notes = JSON.parse(data);
   }
 
+  // Saves notes in memory to file
   async saveNotesToFile() {
     return await fs.writeFile(
       path.join(__dirname, "../db/db.json"),
@@ -28,12 +29,30 @@ class NoteData {
     return this.notes;
   }
 
+  // Saves a note to notes
   async save(note) {
+    // Assign a new random id to the note before storing it
     if (!note.id) {
       note.id = nanoid(ID_LENGTH);
     }
     this.notes.push(note);
     return await this.saveNotesToFile();
+  }
+
+  // Method to delete a note from notes
+  async delete(id) {
+    // Filter out notes that match the id
+    // This is the same as deleting the note from our list of notes
+    const newNotesList = this.notes.filter((note) => {
+      return note.id !== id;
+    });
+    // If the lists are the same length, we didn't delete anything
+    if (this.notes.length === newNotesList.length) {
+      throw new Error(`Note ID not found.`);
+    } else {
+      this.notes = newNotesList;
+      return await this.saveNotesToFile();
+    }
   }
 }
 
